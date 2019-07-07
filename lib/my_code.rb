c053bb2232e9dd4633a5240031f393e8c24ac887
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 00:11:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+array = [1,2,3]
+array.map { |n| n * -2 }
+#end# My Code here....
+def map_to_no_change(array)
+  array.map{ |x| x*1 }
+end
+def map_to_double(array)
+  array.map { |n| n * 2 }
+end
+def map_to_square(array)
+  array.map { |n| n ** 2 }
+end
+
+
+def reduce_to_total(array,starting=0)
+  array.reduce(starting) { |sum,number| sum + number }
+end
+
+def reduce_to_all_true(array)
+  array.reduce(true, :&)
+end
+
+def reduce_to_any_true(array)
+  #array.reduce(true, :&)
+  array.reduce{|x| x = true}
+
+
+end# Your Code Here
